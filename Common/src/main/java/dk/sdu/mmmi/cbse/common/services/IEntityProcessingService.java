9cc8592377f3fdf6476233f9c14950296f3cf3d8
Frederik L. Andersen <frederiklind@hotmail.dk>
2025-03-26 13:36:27 +0100

    Javadocs init

--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
@@ -3,14 +3,21 @@ package dk.sdu.mmmi.cbse.common.services;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
+/**
+ * Interface for game plugin services that can start and stop plugins in the game.
+ *
+ * Implementations of this interface must handle the initialization and cleanup
+ * of game-specific functionality.
+ */
 public interface IEntityProcessingService {
 
     /**
+     * Method for processing game objects during game update loop.
      *
+     * 
      *
-     *
-     * @param gameData
-     * @param world
+     * @param gameData access to display configuration and gamekeys
+     * @param world access to add/remove entities from map
      * @throws
      */
     void process(GameData gameData, World world);
